9079fd377623b656c215a8701a94ca460be81a34
Pedro <pedromarques391@gmail.com>
2024-10-19 12:03:36 -0300

    Atualizando bot

--- src/functions/generalFunctions.js
+++ src/functions/generalFunctions.js
@@ -44,7 +44,22 @@ async function sendAudios(msg, MessageMedia, client) {
   }
 }
 
+function formatDate(unixTimeStamp) {
+  const date = new Date(unixTimeStamp * 1000);
+
+  const year = date.getFullYear();
+  const month = (`0${date.getMonth() + 1}`).slice(-2);
+  const day = (`0${date.getDate()}`).slice(-2);
+  const hours = (`0${date.getHours()}`).slice(-2);
+  const minutes = (`0${date.getMinutes()}`).slice(-2);
+  const seconds = (`0${date.getSeconds()}`).slice(-2);
+
+  const formattedTime = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  console.log(formattedTime);
+}
+
 module.exports = {
   makeSticker,
   sendAudios,
+  formatDate,
 };
