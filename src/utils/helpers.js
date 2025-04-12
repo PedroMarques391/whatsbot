9831f49e6abfd35f30c5f8bbc9bd4c78a3682840
Pedro Marques <pedromarques391@gmail.com>
2025-04-12 00:34:01 -0300

    feat: criando helper para adicionar delay quando necessário.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,6 +7,17 @@ function extractTextFromBody(body) {
     return words.join(' ').trim();
 }
 
+/**
+ * Pausa a execução por um período de tempo especificado.
+ *
+ * @param {number} ms - O número de milissegundos para aguardar.
+ * @returns {Promise<void>} Uma Promise que é resolvida após o tempo especificado.
+ */
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 module.exports = {
     extractTextFromBody,
+    delay,
 };
