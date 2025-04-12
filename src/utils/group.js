83d0df86641051385b8e414a761587b87c550ee1
Pedro Marques <pedromarques391@gmail.com>
2025-04-12 00:33:27 -0300

    feat: criando função para enviar mensagens de atualização em todos os grupos.

--- src/utils/group.js
+++ src/utils/group.js
@@ -1,3 +1,7 @@
+const { MessageMedia } = require('whatsapp-web.js');
+const { delay } = require('./helpers');
+const { adaUpdateMessage } = require('./messages');
+
 /**
  * @description Retorna todos os participantes do grupo.
  */
@@ -9,6 +13,24 @@ function groupParticipants(chat) {
     return participants;
 }
 
+/**
+ * Envia mensagens para uma lista de grupos usando o cliente fornecido.
+ *
+ * @async
+ * @function sendGroupMessages
+ * @param {string[]} groups - Um array de identificadores de grupos para enviar mensagens.
+ * @param {import('wwebjs').Client} client - A instância do cliente WhatsApp Web JS.
+ * @returns {Promise<void>} Resolve quando todas as mensagens forem enviadas.
+ */
+const sendGroupMessages = async (groups, client) => {
+    const media = MessageMedia.fromFilePath('./src/img/adaUpdate.jpg');
+    for (const group of groups) {
+        await client.sendMessage(group, media, { caption: adaUpdateMessage });
+        await delay(2000);
+    }
+};
+
 module.exports = {
     groupParticipants,
+    sendGroupMessages,
 };
