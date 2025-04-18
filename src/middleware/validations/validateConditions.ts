c414ef422d9ab1f37687a60914c8e511f7d911ce
Pedro Marques <pedromarques391@gmail.com>
2025-04-18 11:38:06 -0300

    feat: criando middleware para validar se o comando recebido cumpre as condições para ser ativado.

---
+++ src/middleware/validations/validateConditions.ts
@@ -0,0 +1,10 @@
+import { Chat, Client, Message } from "whatsapp-web.js";
+import { ICommand } from "../../../types";
+
+export function validateConditions(command: ICommand, message: Message, chat: Chat, client: Client): boolean {
+    if (command.conditions && !command.conditions({ message, chat, client })) {
+        console.log(`Comando ${command.name} ignorado por não cumprir condição`);
+        return true
+    }
+    return false
+}
