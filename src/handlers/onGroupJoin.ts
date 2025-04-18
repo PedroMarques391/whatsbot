8942c3ddd4b69fbc505e8f4631af79983c435c58
Pedro Marques <pedromarques391@gmail.com>
2025-04-18 11:28:19 -0300

    feat: criando manipulador de grups, eventos de entrada de usuário são capturados aqui.

---
+++ src/handlers/onGroupJoin.ts
@@ -0,0 +1,6 @@
+import { Client, GroupNotification } from "whatsapp-web.js";
+import { join } from "../services";
+
+export async function onGroupJoin(client: Client, notification: GroupNotification) {
+    await join(notification, client);
+}
