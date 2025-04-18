9b09a189263e4c43c30d4400a0efcd8ff5af2c98
Pedro Marques <pedromarques391@gmail.com>
2025-04-18 11:30:46 -0300

    feat: importando e exportando todos os manipuladores em index.ts.

---
+++ src/handlers/index.ts
@@ -0,0 +1,12 @@
+import { onCall } from "./onCall"
+import { onGroupJoin } from "./onGroupJoin"
+import { onRevoke } from "./onRevoke"
+import { onMessageCreate } from "./onMessageCreate"
+
+
+export {
+    onCall,
+    onGroupJoin,
+    onRevoke,
+    onMessageCreate
+}
