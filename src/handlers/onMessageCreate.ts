f6a22987296e46047e38cba9112d7fe858f5145a
Pedro Marques <pedromarques391@gmail.com>
2025-04-18 11:30:10 -0300

    feat: criando manipulador de mensagem, eventos de mensgens são capturados e tratados aqui, onMessage create é a mente do bot..

---
+++ src/handlers/onMessageCreate.ts
@@ -0,0 +1,32 @@
+import { Chat, Client, Message } from "whatsapp-web.js";
+import { commandHandler } from "../commands";
+import { interactionsHandler, validateCommand, validatorsHandler } from "../middleware";
+
+export async function onMessageCreate(client: Client, message: Message) {
+    const chat: Chat = await message.getChat();
+    const quotedMessage = await message.getQuotedMessage();
+    const contextMessage = message.hasQuotedMsg ? quotedMessage : message;
+    const command = await commandHandler(message.body);
+
+
+    if (await interactionsHandler(message, quotedMessage, chat, client)) return
+
+    console.log(contextMessage)
+
+
+    if (!command) {
+        await validateCommand(message)
+        return true
+    }
+
+    if (await validatorsHandler(command, message, chat, client)) return
+
+
+
+    try {
+        await command.execute({ chat, client, message: contextMessage });
+    } catch (error) {
+        console.error(`Erro ao executar ${command.name}:`, error);
+        await message.reply('🌸 Opsie~ Algo deu errado! Tenta de novo ou avisa meu dev! 🐾');
+    }
+}
