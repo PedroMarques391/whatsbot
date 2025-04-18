360b8d49f929ae1b90bd1eb0c8f45a7f512c607a
Pedro Marques <pedromarques391@gmail.com>
2025-04-18 11:41:08 -0300

    feat: criando funções que vão concentrar os middlewares e que seram chamadas antes de executar os comando principal.

---
+++ src/middleware/index.ts
@@ -0,0 +1,33 @@
+import { Chat, Client, Message } from "whatsapp-web.js";
+import { greeting } from "./interactions/onGreeting";
+import { quotedReply } from "./interactions/onQuotedReply";
+import { ICommand } from "../../types";
+import { validateCommand } from "./validations/validateCommand";
+import { validateOnlyGroup } from "./validations/validateOnlyGroup";
+import { validateConditions } from "./validations/validateConditions";
+import { isViewOnce } from "./interactions/onisViewOnce";
+
+
+
+
+
+async function interactionsHandler(message: Message, quotedMessage: Message, chat: Chat, client: Client): Promise<boolean> {
+    if (await greeting(message, chat)) return true;
+    if (await quotedReply(message, quotedMessage)) return true;
+    if (await isViewOnce(message, client)) return true;
+    return false
+}
+
+async function validatorsHandler(command: ICommand, message: Message, chat: Chat, client: Client): Promise<boolean> {
+    if (await validateOnlyGroup(command, chat, message)) return true
+    if (validateConditions(command, message, chat, client)) return true
+
+    return false
+}
+
+
+export {
+    interactionsHandler,
+    validatorsHandler,
+    validateCommand
+}
