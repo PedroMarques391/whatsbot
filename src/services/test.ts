757a6a10c7a29bc5f5c187fac9e1ddbd29be23be
Pedro Marques <pedromarques391@gmail.com>
2025-04-28 20:44:50 -0300

    feat: adicionando retorno misto para o comando /test.

--- src/services/test.ts
+++ src/services/test.ts
@@ -2,28 +2,8 @@ import { extractTextFromBody } from "../utils/helpers"
 import { Chat, Client, Message } from "whatsapp-web.js";
 
 export async function textFunction(message: Message, chat: Chat, client: Client) {
-    const name = extractTextFromBody(message.body)
-    const quotedMessage = await message.getQuotedMessage()
-
-    if (!message.hasQuotedMsg || quotedMessage.type !== "sticker") {
-        await message.reply("Preciso de uma figurinha para ser renomeada.")
-        return
-    }
-    if (name.length > 20 || name.length === 0) {
-        const reply = name.length > 10 ? "Não acha esse nome muito grande? aprende a ser gente." : "Que criativo, um nome vazio. Não vou fazer isso."
-        await message.reply(reply)
-        return
-    }
-
-    console.log(message)
-    const media = await quotedMessage.downloadMedia()
-
-    await client.sendMessage(chat.id._serialized, media,
-        {
-            sendMediaAsSticker: true,
-            stickerName: name,
-            stickerAuthor: "AdaBot",
-            stickerCategories: ["figs"]
-        })
+    await message.reply("epaa, não estou com nenhuma função teste ativa. 😑")
+        .then(async (message) => await message.react("🌸"))
+    await message.react("✅")
 
 }
