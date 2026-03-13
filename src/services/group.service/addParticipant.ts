b3adf2a2b0ced782be2a1786874e88bdbdc6d771
Pedro Marques <pedromarques391@gmail.com>
2026-03-13 18:17:28 -0300

    refactor: refactor addParticipant, remove regex and enabling the addition of multiple numbers.

--- src/services/group.service/addParticipant.ts
+++ src/services/group.service/addParticipant.ts
@@ -1,25 +1,16 @@
-import {
-  authorIsAdmin,
-  botIsAdmin,
-  groupParticipants,
-  notAValidNumber,
-} from "@/helpers";
+import { authorIsAdmin, botIsAdmin, groupParticipants } from "@/helpers";
 import { extractTextFromBody } from "@/utils";
 import { GroupChat, Message } from "whatsapp-web.js";
 
 export async function addParticipant(message: Message, chat: GroupChat) {
+  type ParticipantResponse = {
+    code: number;
+    message: string;
+    isInviteV4Sent: boolean;
+  };
+
   try {
-    await Promise.all([
-      botIsAdmin(chat),
-      notAValidNumber(message, "/add"),
-      authorIsAdmin(chat, message),
-    ]);
-    const phoneRegex = /^\d{12,}$/;
-    if (!phoneRegex.test(extractTextFromBody(message.body))) {
-      return message.reply(
-        "O número que você me enviou parece ter algum detalhe incorreto. Tente adicionar *55* e remover o digito *9* se tiver.",
-      );
-    }
+    await Promise.all([botIsAdmin(chat), authorIsAdmin(chat, message)]);
     if (
       groupParticipants(chat).includes(
         `${extractTextFromBody(message.body)}@c.us`,
@@ -29,10 +20,41 @@ export async function addParticipant(message: Message, chat: GroupChat) {
         `Noto que esta pessoa já faz parte do nosso grupo. Não precisamos de convites duplicados, certo? ✨`,
       );
     }
-    await chat.addParticipants([`${extractTextFromBody(message.body)}@c.us`]);
+
+    const newMenbers = extractTextFromBody(message.body)
+      .split(" ")
+      .map((num) => `${num}@c.us`);
+
+    console.log(newMenbers);
+    await chat.addParticipants([...newMenbers]).then((participants) => {
+      Object.entries(participants).forEach(
+        ([number, data]: [string, ParticipantResponse]) => {
+          console.log(number, data);
+          if (data.isInviteV4Sent) {
+            message.reply(
+              `Convite enviado com sucesso para ${number.replace("@c.us", "")}!`,
+            );
+            return;
+          }
+          if ([403, 408, 404].includes(data.code)) {
+            message.reply(
+              `error to add ${number.replace("@c.us", "")}: ${data.message}`,
+            );
+            return;
+          }
+
+          if (data.code === 400) {
+            message.reply(
+              "O número que você me enviou parece ter algum detalhe incorreto. Tente adicionar *55* e remover o digito *9* se tiver.",
+            );
+            return;
+          }
+        },
+      );
+    });
+
     return;
   } catch (error: any) {
     await message.reply(error.message);
-    return;
   }
 }
