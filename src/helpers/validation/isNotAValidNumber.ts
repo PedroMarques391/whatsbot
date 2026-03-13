186d656c825bdb944e6a059988cddfb37ebb2c57
Pedro Marques <pedromarques391@gmail.com>
2026-03-13 18:17:50 -0300

    lint: apply eslint format.

--- src/helpers/validation/isNotAValidNumber.ts
+++ src/helpers/validation/isNotAValidNumber.ts
@@ -1,9 +1,17 @@
-import { extractTextFromBody } from '@/utils';
-import { Message } from 'whatsapp-web.js';
+import { extractTextFromBody } from "@/utils";
+import { Message } from "whatsapp-web.js";
 
-export async function notAValidNumber(message: Message, command: string): Promise<boolean> {
-    if (extractTextFromBody(message.body) === '' || extractTextFromBody(message.body).match(/[+\-()]/)) {
-        throw new Error(`Faltou um pequeno detalhe. Indique o usuário usando '@' logo após o comando '${command}'. ☕`);
-    }
-    return false;
+export async function notAValidNumber(
+  message: Message,
+  command: string,
+): Promise<boolean> {
+  if (
+    extractTextFromBody(message.body) === "" ||
+    extractTextFromBody(message.body).match(/[+\-()]/)
+  ) {
+    throw new Error(
+      `Faltou um pequeno detalhe. Indique o usuário usando '@' logo após o comando '${command}'. ☕`,
+    );
+  }
+  return false;
 }
