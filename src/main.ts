15b60f9b92e66794f3bb61e4c52dc18776a39485
Pedro Marques <pedromarques391@gmail.com>
2026-03-22 21:43:37 -0300

    feat: add  closure control to the main.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import "module-alias/register";
 import { client, connections, eventHandler } from "./bot";
+import { safeShutdown } from "./utils";
 
-
-function bootstrap() {
-    connections(client);
-eventHandler(client);
-client.initialize();
+async function bootstrap(): Promise<void> {
+  connections(client);
+  eventHandler(client);
+  await client.initialize();
 }
 
 bootstrap();
+
+process.on("SIGINT", safeShutdown);
+process.on("SIGTERM", safeShutdown);
+process.on("SIGUSR2", safeShutdown);
