2d396cf5b0695da964446b7e01d5b127dc08860b
Pedro Marques <pedromarques391@gmail.com>
2026-03-25 09:26:25 -0300

    feat: add new index to group schema.

--- src/models/group.ts
+++ src/models/group.ts
@@ -3,19 +3,25 @@ export interface IGroup extends Document {
   groupId: string;
   name: string;
   description?: string;
+  welcomeMessage?: string;
+  leftMessage?: string;
   members: string[];
   blockedCommands: string[];
   createdAt: Date;
   updatedAt: Date | null;
+  leftAt: Date | null;
 }
 const GroupSchema = new Schema<IGroup>({
   groupId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   description: { type: String, required: false },
+  welcomeMessage: { type: String, required: false },
+  leftMessage: { type: String, required: false },
   members: [{ type: String, default: [] }],
   blockedCommands: [{ type: String, default: [] }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: null },
+  leftAt: { type: Date, default: null },
 });
 const GroupModel = model<IGroup>("Group", GroupSchema);
 export default GroupModel;
